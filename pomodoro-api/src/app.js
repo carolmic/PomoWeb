b07038c12313cd32d51284c0305dadb46b65cd57
Carolina Ferreira <carolina.ferreira@poatek.com>
2024-07-22 10:27:58 -0300

    add get all endpoint

--- pomodoro-api/src/app.js
+++ pomodoro-api/src/app.js
@@ -1,48 +1,58 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import gridfsStream from 'gridfs-stream';
-import mongoose from 'mongoose';
-import multer from 'multer';
-import { GridFsStorage } from 'multer-gridfs-storage';
-import db from './config/dbConnect.js';
+import dotenv from "dotenv";
+import express from "express";
+import gridfsStream from "gridfs-stream";
+import mongoose from "mongoose";
+import multer from "multer";
+import { GridFsStorage } from "multer-gridfs-storage";
+import db from "./config/dbConnect.js";
 
 // Carregar variáveis de ambiente do arquivo .env
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 const storage = new GridFsStorage({
-  url: process.env.MONGODB_URI,
-  file: (req, file) => {
-    return {
-      filename: file.originalname
-    };
-  }
+	url: process.env.MONGODB_URI,
+	file: (req, file) => {
+		return {
+			filename: file.originalname,
+		};
+	},
 });
 
 // Configurar o GridFS Stream
 gridfsStream.mongo = mongoose.mongo;
 let gfs;
 
-db.once('open', () => {
-  gfs = gridfsStream(db.db, mongoose.mongo);
-  gfs.collection('audio');
+db.once("open", () => {
+	gfs = gridfsStream(db.db, mongoose.mongo);
+	gfs.collection("audio");
 });
 
 const upload = multer({ storage });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/upload_files', upload.single('file'), (req, res) => {
-  res.send({ message: 'Successfully uploaded files' })
+app.post("/upload_files", upload.single("file"), (req, res) => {
+	res.send({ message: "Successfully uploaded files" });
+});
+
+app.get("/files", async (req, res) => {
+	try {
+		let files = await db.collection("fs.files").find().toArray();
+		res.json({ files });
+	} catch (err) {
+    console.error(err);
+		res.json({ err });
+	}
 });
 
 app.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+	console.log("Server running at http://localhost:3000");
 });
 
 export default app;
