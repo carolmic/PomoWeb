dd7f549980fa76169c12a23e7785f0d673c973e1
Carolina Ferreira <carolina.ferreira@poatek.com>
2024-07-24 16:55:05 -0300

    remove unecessary lines from app.js

--- pomodoro-api/app.js
+++ pomodoro-api/app.js
@@ -1,9 +1,6 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import mongodb from "mongodb";
-import mongoose from "mongoose";
-import db from "./src/config/dbConnect.js";
 import endpoints from "./src/routes/endpoints.js";
 import swaggerSetup from "./swagger.js";
 
@@ -15,14 +12,6 @@ app.use(express.static("public"));
 app.use(cors());
 app.use("/api", endpoints);
 app.use("/api-docs", swaggerSetup);
-
-let gfsBucket;
-db.once("open", () => {
-	const connection = mongoose.connection.db;
-	gfsBucket = new mongodb.GridFSBucket(connection, {
-		bucketName: "fs",
-	});
-});
 
 app.listen(8080, () => {
 	console.log("Server running at http://localhost:8080");
