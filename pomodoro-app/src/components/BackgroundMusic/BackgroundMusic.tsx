df78cb5c20169d275594cf645a1d79b6fc097d55
Carolina Ferreira <carolina.ferreira@poatek.com>
2024-07-30 11:03:19 -0300

    remove integration from frontend

--- pomodoro-app/src/components/BackgroundMusic/BackgroundMusic.tsx
+++ pomodoro-app/src/components/BackgroundMusic/BackgroundMusic.tsx
@@ -1,71 +1,22 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef } from 'react';
+import lofi from '../../assets/summer-rain-lofi.mp3';
 
 interface BackgroundMusicProps {
 	play: boolean;
 }
 
-const BackgroundMusic = ({ play }: BackgroundMusicProps) => {
-	const [files, setFiles] = useState<any[]>([]);
-	const [currentAudio, setCurrentAudio] = useState<string>("");
-	const baseUrl = "http://pomodoro-api.eba-psvtrnjk.us-east-2.elasticbeanstalk.com/download/";
+const BackgroundMusic = ({ play }: BackgroundMusicProps ) => {
+  const audioRef = useRef<HTMLAudioElement | null>(null);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await fetch("http://pomodoro-api.eba-psvtrnjk.us-east-2.elasticbeanstalk.com/files");
-				const data = await response.json();
-				const filesWithUrls = data.files.map((file: any) => ({
-					...file,
-					url: `${baseUrl}${file.filename}`,
-				}));
-				setFiles(filesWithUrls);
-				setCurrentAudio(filesWithUrls[1].url);
-			} catch (error) {
-				console.error("Error fetching data:", error);
-			}
-		};
+	if (play && audioRef.current) {
+		audioRef?.current?.play();
+	} else {
+		audioRef?.current?.pause();
+	}
 
-		fetchData();
-	}, []);
-
-	const audioRef = useRef<HTMLAudioElement | null>(null);
-
-	useEffect(() => {
-		if (play) {
-			audioRef.current?.play();
-		} else {
-			audioRef.current?.pause();
-		}
-	}, [play]);
-
-	const handleSkip = () => {
-		setCurrentAudio(files.sort(() => Math.random() - 0.5)[0].url);
-	};
-
-	useEffect(() => {
-		let interval = setInterval(() => {
-			if (audioRef.current?.ended) {
-				handleSkip();
-				if (currentAudio) {
-					audioRef.current?.load();
-					audioRef.current?.play();
-				}
-			}		
-			interval;
-		}, 1000);
-	}, [currentAudio]);
-
-	return (
-		<>
-			{currentAudio && (
-				<>
-					<audio ref={audioRef} autoPlay>
-						<source src={currentAudio} type="audio/mpeg" />
-					</audio>
-				</>
-			)}
-		</>
-	);
+  return (
+		<audio ref={audioRef} src={lofi} loop />
+  );
 };
 
 export default BackgroundMusic;
